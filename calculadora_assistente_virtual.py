ca3747790152c094062adecd34125acea1e36bc1
Leonardo Stella de Oliveira <Leonstella97@gmail.com>
2023-07-13 13:18:18 -0300

    allow-empty

--- calculadora_assistente_virtual.py
+++ calculadora_assistente_virtual.py
@@ -18,3 +18,18 @@ def calcular(operador, num1, num2):
     return resultado
 
 falar("Olá! Sou uma calculadora virtual. Como posso ajudar?")
+
+while True:
+    entrada = input("Digite a operação matemática (ou 'sair' para encerrar): ")
+    if entrada.lower() == 'sair':
+        falar("Até logo!")
+        break
+    
+    partes = entrada.split()
+    if len(partes) != 3:
+        falar("Entrada inválida. Por favor, tente novamente.")
+        continue
+    
+    operador, num1, num2 = partes
+    num1 = float(num1)
+    num2 = float(num2)
